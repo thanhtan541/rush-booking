800f0f603d8ba8388b7276cbb0b3e618039ef018
thanhtan541 <thanhtan004100@gmail.com>
2024-07-26 17:20:57 +0700

    feat: add cors

--- src/startup.rs
+++ src/startup.rs
@@ -1,3 +1,4 @@
+use actix_cors::Cors;
 use actix_web::{
     dev::Server,
     web::{self, Data},
@@ -58,10 +59,20 @@ async fn run(
     let base_url = Data::new(ApplicationBaseUrl(base_url));
     let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:8080")
+            // .allowed_origin_fn(|origin, _req_head| {
+            //     origin.as_bytes().ends_with(b".rust-lang.org")
+            // })
+            // .allowed_methods(vec!["GET", "POST"])
+            // .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+            // .allowed_header(http::header::CONTENT_TYPE)
+            .max_age(3600);
         App::new()
             // Logger middleware
             // Sent active-web log to log subscriber
             .wrap(TracingLogger::default())
+            .wrap(cors)
             .service(health_check)
             .service(
                 web::scope("/admin")
