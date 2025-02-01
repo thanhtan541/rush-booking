72d0ea055a74254c33564c5610605e010f6c1236
nguyen thanh tan <thanhtan004100@gmail.com>
2025-02-01 12:32:26 +0700

    feat: try test opt out harness

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -11,9 +11,10 @@ fi
 if ! [ -x "$(command -v sqlx)" ]; then
   echo >&2 "Error: sqlx is not installed."
   echo >&2 "Use:"
-  echo >&2 "    cargo install --version='~0.7' sqlx-cli \
+  echo >&2 "    cargo install --version='~1.7' sqlx-cli \
 --no-default-features --features rustls,postgres"
   echo >&2 "to install it."
+  exit 1
 fi
 
 DB_USER="${POSTGRES_USER:=postgres}"
